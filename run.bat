21096d29eae5eb62ac1b8c8e7da79069d9946ed1
Limitex <76650151+Limitex@users.noreply.github.com>
2022-11-16 02:20:02 +0900

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,29 +1,23 @@
 @echo off
 
-:: UNCOMMENT (remove ::) if you want the backend to automatically save files for you
-:: export SAVE_FILES="1"
-
 if not defined PYTHON (set PYTHON=python)
 if not defined VENV_DIR (set VENV_DIR=venv)
 
 set ERROR_REPORTING=FALSE
-
 set PYTHON="python"
-
 set DTYPE=float32
 set CLIP_CONTEXTS=3
 set AMP=1
 set MODEL=stable-diffusion
 set DEV=True
 set MODEL_PATH=models/animefull-final-pruned
-::these aren't actually used by the site?
-::set MODULE_PATH=models/modules
-::unclear if these are used either
-::set PRIOR_PATH=models/vector_adjust_v2.pt
 set ENABLE_EMA=1
 set VAE_PATH=models/animevae.pt
 set PENULTIMATE=1
 set PYTHONDONTWRITEBYTECODE=1
+
+::set MODULE_PATH=models/modules
+::set PRIOR_PATH=models/vector_adjust_v2.pt
 
 %PYTHON% -m uvicorn --host 127.0.0.1 --port=80 --workers 1 main:app
 pause
